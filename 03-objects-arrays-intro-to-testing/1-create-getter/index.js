495fb2c37094c53f7b37e15805d9a5b38dda825d
alexeybogdanov <aleksei.n.bogdanov@gmail.com>
2020-12-20 18:06:12 +0100

    task3 part 1

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,12 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
+    // const pathArray =  path.split('.');
 
+   return function getNestedValue(obj, key) {
+        return path.split(".").reduce(function(result, key) {
+           return result[key] 
+        }, obj);
+    }
+     
 }
