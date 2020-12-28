4628e7833453aee28f7a14b5edb3901b45c63ffb
alexeybogdanov <aleksei.n.bogdanov@gmail.com>
2020-12-28 18:26:48 +0100

    task5-1 notification

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,56 @@
 export default class NotificationMessage {
+    openItems = [];
 
+    constructor(message = 'default text', {
+        duration = '',
+        type = ''
+    } = {}) {
+
+        this.message = message;
+        this.duration = duration;
+        this.type = type;
+        this.render();
+    }
+
+    render() {
+        const element = document.createElement('div');
+        element.innerHTML = this.notificationBody;
+        this.element = element.firstElementChild;
+        this.openItems.push(this.element)
+    }
+
+    show(target) {
+
+        document.querySelectorAll('.notification').forEach(e => e.parentNode.removeChild(e));
+        //this.openItems.forEach(e => e.parentNode.removeChild(e));   //не могу добиться чтобы это работало
+    
+        if (target) {
+            target.appendChild(this.element);
+        }
+        else {
+            document.body.appendChild(this.element);
+        }
+        setTimeout(function () { this.remove() }.bind(this), this.duration);
+    }
+
+    get notificationBody() {
+        return `
+      <div class="notification ${this.type}" style="--value:${this.duration}ms">
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+          <div class="notification-header">${this.type}</div>
+          <div class="notification-body">${this.message}
+          </div>
+        </div>
+      </div>
+        `;
+    }
+
+    remove() {
+        this.element.remove();
+    }
+
+    destroy() {
+        this.remove();
+    }
 }
